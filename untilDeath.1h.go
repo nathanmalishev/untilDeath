bd1427233c82878ba2f9e6d99f6a6cede4bfde8d
Nathan Malishev <nathan.malishev@gmail.com>
2017-09-30 23:09:43 +1000

    program added

---
+++ untilDeath.1h.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"time"
+)
+
+func main() {
+	const t2death = "3366186454" // die 2076 september 1st
+	i, _ := strconv.ParseInt(t2death, 10, 64)
+
+	t := time.Unix(i, 0)
+	tUntilDeath := time.Until(t)
+
+	yrs := tUntilDeath.Hours() / (365 * 24)
+	rYrs := math.Floor(yrs)
+
+	days := (yrs - rYrs) * (365)
+	rDays := math.Floor(days)
+
+	hours := (days - rDays) * (24)
+	rHours := math.Floor(hours)
+
+	mins := (hours - rHours) * 60
+	rMins := math.Floor(mins)
+
+	fmt.Printf("%+vyrs, %+vdays, %+vhours, %+vminutes", rYrs, rDays, rHours, rMins)
+}
